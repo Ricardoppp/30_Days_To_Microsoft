55d070da39b145fa01eb3e7400671305082d34fa
Ricardo Prieto <ricardo_19_pr@hotmail.com>
2024-08-15 18:20:16 -0500

    Update 15/08

--- Ejercicios_30Days.js
+++ Ejercicios_30Days.js
@@ -39,13 +39,30 @@
     console.log(challenge.replace('JavaScript', 'Python'))
 
 // What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+    console.log(challenge.charAt(15))
+
 // What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+    console.log(challenge.charCodeAt(11))
+
 // Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
-// Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
-// Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-// Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-// Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+    console.log(challenge.indexOf('a'))
+
+// Use lastIndexOf to determine the position of the last occurrence of 'a' in 30 Days Of JavaScript.
+    console.log(challenge.lastIndexOf('a'))
+
+// Use indexOf to find the position of the first occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+    let string = "You cannot end a sentence with because because because is a conjunction"
+
+    console.log(string.indexOf('because'))
+
+// Use lastIndexOf to find the position of the last occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+    console.log(string.lastIndexOf('because'))
+
+// Use search to find the position of the first occurrence of the word 'because' in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+    console.log(string.search('because'))
+
 // Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+
 // Use startsWith() method with the string 30 Days Of JavaScript and make the result true
 // Use endsWith() method with the string 30 Days Of JavaScript and make the result true
 // Use match() method to find all the a’s in 30 Days Of JavaScript
