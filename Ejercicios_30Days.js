1a5d0ad7fff059e9935d9e10e22eec536b16e104
Ricardo Prieto <ricardo_19_pr@hotmail.com>
2024-08-16 00:58:46 -0500

    Update 16/08

--- Ejercicios_30Days.js
+++ Ejercicios_30Days.js
@@ -80,3 +80,85 @@
 
 // Use repeat() method to print 30 Days Of JavaScript 2 times
     console.log(challenge.repeat(2))
+
+
+//* Exercise: Level 2
+
+// Using console.log() print out the following statement:
+// The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+    let quote = 'There is no exercise better for the heart than reaching down and lifting people up.'
+    let by = 'John Holmes'
+
+    console.log(quote)
+
+// Using console.log() print out the following quote by Mother Teresa:
+// "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+    let quote2 = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+    let by2= "Mother Teresa"
+    console.log(quote2)
+
+// Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+    let ten = '10'
+    console.log(typeof ten)
+
+    let trans = parseInt(ten)
+    console.log(typeof trans)
+    console.log(trans == 10)
+
+// Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+    let check = '9.8'
+    console.log(9.8 == 10)
+    console.log(Math.round(check))
+
+// Check if 'on' is found in both python and jargon
+    let on = 'python and jargon'
+    console.log(on.includes('on'))
+
+// I hope this course is not full of jargon. Check if jargon is in the sentence.
+    let jargon = 'I hope this course is not full of jargon'
+    console.log(jargon.includes('jargon'))
+
+// Generate a random number between 0 and 100 inclusively.
+    const numberr = Math.floor(Math.random() * 101)
+    console.log(numberr)
+
+// Generate a random number between 50 and 100 inclusively.
+    function randomm(min,max){
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random()* (max-min+1)+min)
+    }
+    let randommm = randomm(50, 101)
+    console.log(randommm)
+
+// Generate a random number between 0 and 255 inclusively.
+    const numberr2 = Math.floor(Math.random() * 256)
+    console.log(numberr2)
+
+// Access the 'JavaScript' string characters using a random number.
+    // TODO: NO entendi ni verga we
+
+// Use console.log() and escape characters to print the following pattern.
+// 1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125
+    // TODO: Este esta peor mk, no entendi nada
+
+// Use substr to slice out the phrase 'because because because' from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+    let cut = 'You cannot end a sentence with because because because is a conjunction'
+
+    console.log(cut.substring(31,53))
+    // TODO: Este me deja una duda existencial
+
+
+//* Exercises: Level 3
+// 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+
+// Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+// Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+//     const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+// Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
