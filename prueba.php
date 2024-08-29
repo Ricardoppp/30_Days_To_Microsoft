9061f91d33ab67bcbe013c29a83eb7d521f156a5
Ricardo Prieto <ricardo_19_pr@hotmail.com>
2024-08-28 22:56:40 -0500

    Create prueba.php

---
+++ prueba.php
@@ -0,0 +1,19 @@
+<?
+    echo "Hola, esto es una prueba, de como se usa PHP, ya que siempre se trabaja con XAMPP \n
+    y la verdad me da pereza migrar todo el proyecto para allá <br>" ;
+    
+    echo "estoy pensando que probar los dos lados, por el curso, pero mucha tarea la madre";
+
+    //* Tipos de datos
+    // Enteros
+    $edad=26;
+    // Flotanes
+    $altura= 1.76;
+    // Strings
+    $mensaje="vas a llorar, Ricardo";
+    /*booleanos
+    True or false
+    */
+
+    
+?>
